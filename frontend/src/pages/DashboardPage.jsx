e497c6d180649b986bd0a9e385e997f5e89ff1bb
Shashintha24 <e22125@eng.pdn.ac.lk>
2026-03-09 23:57:50 +0530

    file stureture

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DashboardPage() {
+    return (
+        <div>
+            <h1>Dashboard Page</h1>
+            <p>Welcome to the dashboard!</p>
+        </div>
+    );
+}
+export default DashboardPage;
