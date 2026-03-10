1f416a8ec8472050cb481e7ea9ab6c067d904d12
Shashintha24 <e22125@eng.pdn.ac.lk>
2026-03-10 07:47:29 +0530

    initial frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,29 +1,59 @@
 import React from "react";
 
 function HomePage() {
+    document.addEventListener('DOMContentLoaded', () => {
+            // Navbar js
+            var toggleOpen = document.getElementById('toggleOpen');
+            var toggleClose = document.getElementById('toggleClose');
+            var collapseMenu = document.getElementById('collapseMenu');
+
+            function handleClick() {
+                if (collapseMenu.style.display === 'block') {
+                    collapseMenu.style.display = 'none';
+                } else {
+                    collapseMenu.style.display = 'block';
+                }
+            }
+
+            toggleOpen.addEventListener('click', handleClick);
+            toggleClose.addEventListener('click', handleClick);
+
+            // FAQ js
+            document.querySelectorAll('.accordion').forEach(elm => {
+                const button = elm.querySelector('.toggle-button');
+                const content = elm.querySelector('.content');
+                const plusIcon = button.querySelector('.plus');
+
+                button.addEventListener('click', () => {
+                    const isHidden = content.classList.toggle('invisible');
+                    content.style.maxHeight = isHidden ? '0px' : `${content.scrollHeight + 100}px`;
+                    content.classList.toggle('pb-4', !isHidden);
+                    plusIcon.classList.toggle('hidden', !isHidden);
+                    plusIcon.classList.toggle('block', isHidden);
+                });
+            });
+
+            // scroll
+            document.querySelectorAll("a[href^='#']").forEach(anchor => {
+                anchor.addEventListener("click", function (e) {
+                    e.preventDefault();
+
+                    const targetId = this.getAttribute("href");
+                    const target = document.querySelector(targetId);
+                    const offset = 70; // control scroll position
+
+                    if (target) {
+                        window.scrollTo({
+                            top: target.offsetTop - offset,
+                            behavior: "smooth"
+                        });
+                    }
+                });
+            });
+
+        });
   return (
   <div>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src='https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4'></script>
-
-    <!-- google font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
-
-    <style>
-        body {
-            font-family: Poppins, sans-serif;
-        }
-    </style>
-
-</head>
 
 <div className="max-w-[1920px] mx-auto">
     <div className="bg-white text-slate-900 text-[15px]">
@@ -1372,61 +1402,6 @@ function HomePage() {
 
     </div>
 
-    <script>
-
-        document.addEventListener('DOMContentLoaded', () => {
-            // Navbar js
-            var toggleOpen = document.getElementById('toggleOpen');
-            var toggleClose = document.getElementById('toggleClose');
-            var collapseMenu = document.getElementById('collapseMenu');
-
-            function handleClick() {
-                if (collapseMenu.style.display === 'block') {
-                    collapseMenu.style.display = 'none';
-                } else {
-                    collapseMenu.style.display = 'block';
-                }
-            }
-
-            toggleOpen.addEventListener('click', handleClick);
-            toggleClose.addEventListener('click', handleClick);
-
-            // FAQ js
-            document.querySelectorAll('.accordion').forEach(elm => {
-                const button = elm.querySelector('.toggle-button');
-                const content = elm.querySelector('.content');
-                const plusIcon = button.querySelector('.plus');
-
-                button.addEventListener('click', () => {
-                    const isHidden = content.classList.toggle('invisible');
-                    content.style.maxHeight = isHidden ? '0px' : `${content.scrollHeight + 100}px`;
-                    content.classList.toggle('pb-4', !isHidden);
-                    plusIcon.classList.toggle('hidden', !isHidden);
-                    plusIcon.classList.toggle('block', isHidden);
-                });
-            });
-
-            // scroll
-            document.querySelectorAll("a[href^='#']").forEach(anchor => {
-                anchor.addEventListener("click", function (e) {
-                    e.preventDefault();
-
-                    const targetId = this.getAttribute("href");
-                    const target = document.querySelector(targetId);
-                    const offset = 70; // control scroll position
-
-                    if (target) {
-                        window.scrollTo({
-                            top: target.offsetTop - offset,
-                            behavior: "smooth"
-                        });
-                    }
-                });
-            });
-
-        });
-
-    </script>
 </div>
 </div>
 
