c8aaecc79a8a397fdb21bdea45ac5d41937b9f31
Shashintha24 <e22125@eng.pdn.ac.lk>
2026-04-12 09:57:28 +0530

    fix the

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
-import AdminPage from './pages/AdminPage'
-import CoursePage from './pages/CoursePage'
-import DashboardPage from './pages/DashboardPage'
-import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import NotFoundPage from './pages/NotFoundPage'
 import RegisterPage from './pages/RegisterPage'
@@ -15,14 +11,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/courses" element={<CoursePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/admin" element={<AdminPage />} />
+       
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/not-found" element={<NotFoundPage />} />
-        <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="/student" element={<Studentpage />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/management" element={<Manegement />} />
